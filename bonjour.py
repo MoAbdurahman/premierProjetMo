5e1f94540cefeb2148b5c33d56e00b278575cf0b
Abdurahman Mohamed (HES) <mohamed.abdurahm@hes.adhes.hesge.ch>
2019-09-18 11:33:55 +0200

    maDeuxiemeSauvegarde

--- bonjour.py
+++ bonjour.py
@@ -9,3 +9,6 @@ résultat : affiche un message de bienvenue aux etudiant-e-semestre
 
 print("Bonjour !")
 print("bienvenue aux etudiant-e-s  2019-2020 !")
+print("Informatique de gestion")
+print("1er semestre 2019-2020")
+print("Bonjour HEG")
