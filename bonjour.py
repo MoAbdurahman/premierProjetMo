3e2380776349d3d5b84e94e7996c286165dc8bd8
Abdurahman Mohamed (HES) <mohamed.abdurahm@hes.adhes.hesge.ch>
2019-09-18 10:51:37 +0200

    description sauvegarde

---
+++ bonjour.py
@@ -0,0 +1,11 @@
+""" Bienvenue
+données semestre d'entrées
+résultat : affiche un message de bienvenue aux etudiant-e-semestre
+"""
+
+### declaration des variables 
+### initialisation des variables 
+### séquence d'opérations
+
+print("Bonjour !")
+print("bienvenue aux etudiant-e-s  2019-2020 !")
